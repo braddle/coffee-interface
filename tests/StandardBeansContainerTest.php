7bd760bc43229fdb63a168bb0d4a417c21d63798
braddle <braddle@gmail.com>
2012-10-06 08:37:47 +0100

    Adding folder for test and a class to test Beans Containers.

---
+++ tests/StandardBeansContainerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *
+ * @author Mark Bradley<mark@mark-bradley.net>
+ */
+class StandardBeansContainerTest  extends PHPUnit_Framework_TestCase
+{
+
+	/**
+	 *
+	 * @author Mark Bradley<mark@mark-bradley.net>
+	 * @returns null
+	 */
+	public function ()
+	{
+	}
+
+
+} // StandardBeansContainerTest
